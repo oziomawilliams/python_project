cce58a442068de82c7d668990fb5eecfd538723b
oziomawilliams <oziomaosademe@gmail.com>
2025-09-07 15:57:19 +0100

    Discount Calculator

---
+++ practice.py
@@ -0,0 +1,8 @@
+def divisible_by_10 (num):
+    if num % 10 == 0:
+        return True
+    else:
+        return False
+    
+print(divisible_by_10(20))  # True
+print(divisible_by_10(25))  # False
